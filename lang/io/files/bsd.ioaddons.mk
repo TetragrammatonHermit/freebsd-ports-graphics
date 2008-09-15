6bdf6727597c6fa6504069204c78b720042fe822
Martin Wilke <miwi@FreeBSD.org>
2008-09-15 22:55:28 +0000

    - better way to us the addones.

--- lang/io/files/bsd.ioaddons.mk
+++ lang/io/files/bsd.ioaddons.mk
@@ -7,50 +7,41 @@
 
 IOVERSION?=	20080904
 
-_USE_IO= 	blowfish
 
+WRKSRC?=		${WRKDIR}/io
+DEFAULT_ADDONS?=	${PORTNAME}
+TARGET_DIR= 	${PREFIX}/lib/io/addons/${DEFAULT_ADDONS}
+SRC_DIR= 	${WRKSRC}/addons/${DEFAULT_ADDONS}
 
-blowfish_RUN_DEPENDS=	${PREFIX}/lib/io/addons/Blowfish/io/Blowfish.io${PORTSDIR}/security/io-blowfish
-
-WRKSRC=			${WRKDIR}/io
-
-TARGET_DIR= ${PREFIX}/lib/io/addons/${ADDON_NAME}
-SRC_DIR= ${WRKSRC}/addons/${ADDON_NAME}
-
-HEADER_PATHS=	append("${PREFIX}/include/io")
-LIB_PATHS=
-.for i in ${ADDON_DEPENDS_ON_ADDONS}
-HEADER_PATHS+=	append("${PREFIX}/include/io/${i}")
-LIB_PATHS+=	append("${PREFIX}/lib/io/addons/${i}/_build/dll")
-.endfor
-
+post-patch:
+	cd ${WRKSRC}/addons && for addon in *; do \
+	if ! ${ECHO_CMD} ${DEFAULT_ADDONS} | ${GREP} $$addon > /dev/null; then \
+	                              ${RM} -fr $$addon; \
+	fi; \
+ done
 
 do-build:
-	cd ${WRKSRC} && ${PREFIX}/bin/io build.io -a ${ADDON_NAME}
+	cd ${WRKSRC}; ${SETENV} ${LOCALBASE}/bin/io build.io -a ${DEFAULT_ADDONS}
 
 do-install:
-	${INSTALL_DATA_DIR} ${TARGET_DIR}
-		${INSTALL_DATA_DIR} ${TARGET_DIR}/_build
+	${MKDIR} ${TARGET_DIR}
+	${MKDIR} ${TARGET_DIR}/_build
 	if [ -d  ${SRC_DIR}/_build/dll ]; then \
 		${CP} -rf ${SRC_DIR}/_build/dll ${TARGET_DIR}/_build/; \
 	fi
 	if [ -d ${SRC_DIR}/_build/lib ]; then \
 		${CP} -rf ${SRC_DIR}/_build/lib ${TARGET_DIR}/_build/; \
 	fi
+	if [ -d ${SRC_DIR}/_build/objs ]; then \
+		${CP} -rf ${SRC_DIR}/_build/objs ${TARGET_DIR}/_build/; \
+	fi
+	if [ -d ${SRC_DIR}/_build/headers ]; then \
+		${CP} -rf ${SRC_DIR}/_build/headers ${TARGET_DIR}/_build/; \
+	fi
 	if [ -d ${SRC_DIR}/io ]; then \
 		${CP} -rf ${SRC_DIR}/io ${TARGET_DIR}/; \
 	fi
-	if [ -e ${SRC_DIR}/depends ]; then \
-		${CP} -f ${SRC_DIR}/depends ${TARGET_DIR}/; \
-	fi
-	if [ -e ${SRC_DIR}/protos ]; then \
-		${CP} -f ${SRC_DIR}/protos ${TARGET_DIR}/; \
-	fi
 	if [ -e ${SRC_DIR}/build.io ]; then \
 		${CP} -f ${SRC_DIR}/build.io ${TARGET_DIR}/; \
-	fi
-	if [ ! -z "${ADDON_INSTALL_HEADERS}" ]; then \
-		${INSTALL_DATA_DIR} ${PREFIX}/include/io/${ADDON_NAME}; \
-		${CP} -f ${SRC_DIR}/source/*.h ${PREFIX}/include/io/${ADDON_NAME}
 	fi
 
