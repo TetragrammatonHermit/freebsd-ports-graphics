c60789de788f15893ab37b7ae3399b4baaa9b6ce
Martin Wilke <miwi@FreeBSD.org>
2008-09-16 13:11:14 +0000

    -  Fix whitespaces - Replace TARGET_DIR -> ADDON_DIR (better to read) - Replease SRC_DIR -> BUILD_DIR (better to read) - Respect ${X11BASE} - Fix HEADER_PATH - Fix compieler warnings

--- lang/io/files/bsd.ioaddons.mk
+++ lang/io/files/bsd.ioaddons.mk
@@ -1,5 +1,5 @@
 #
-# 
+#
 #
 #
 #
@@ -10,48 +10,55 @@ IOVERSION?=	20080904
 IO_BIN=		${LOCALBASE}/bin/io
 IO_PORT=	${PORTSDIR}/lang/io
 
-BUILD_DEPENDS+=	${IO_BIN}:${IO_PORT} 
+BUILD_DEPENDS+=	${IO_BIN}:${IO_PORT}
 
 WRKSRC?=		${WRKDIR}/io
 DEFAULT_ADDONS?=	${PORTNAME}
-TARGET_DIR= 	${PREFIX}/lib/io/addons/${DEFAULT_ADDONS}
-SRC_DIR= 	${WRKSRC}/addons/${DEFAULT_ADDONS}
+ADDON_DIR= 	${PREFIX}/lib/io/addons/${DEFAULT_ADDONS}
+BUILD_DIR= 	${WRKSRC}/addons/${DEFAULT_ADDONS}
+HEADER_PATHS=	append("${PREFIX}/include/io")
+
 
 post-patch:
 	cd ${WRKSRC}/addons && for addon in *; do \
 	if ! ${ECHO_CMD} ${DEFAULT_ADDONS} | ${GREP} $$addon > /dev/null; then \
 	                              ${RM} -fr $$addon; \
 	fi; \
- done
+	done
+	@${REINPLACE_CMD} -e '/headerSearchPaths := List clone/s|$$| ${HEADER_PATHS}|' \
+		    -e 's|/usr/X11R6|${LOCALBASE}|g' \
+		${WRKSRC}/build/AddonBuilder.io
+	@${REINPLACE_CMD} -e 's|CFLAGS = $$(OPTIMIZE)|CFLAGS +=|' \
+	    ${WRKSRC}/Makefile.lib
 
 do-build:
 	cd ${WRKSRC}; ${SETENV} ${LOCALBASE}/bin/io build.io -a ${DEFAULT_ADDONS}
 
 do-install:
-	${MKDIR} ${TARGET_DIR}
-	${MKDIR} ${TARGET_DIR}/_build
-	if [ -d  ${SRC_DIR}/_build/dll ]; then \
-		${CP} -rf ${SRC_DIR}/_build/dll ${TARGET_DIR}/_build/; \
+	${MKDIR} ${ADDON_DIR}
+	${MKDIR} ${ADDON_DIR}/_build
+	if [ -d  ${BUILD_DIR}/_build/dll ]; then \
+		${CP} -rf ${BUILD_DIR}/_build/dll ${ADDON_DIR}/_build/; \
 	fi
-	if [ -d ${SRC_DIR}/_build/lib ]; then \
-		${CP} -rf ${SRC_DIR}/_build/lib ${TARGET_DIR}/_build/; \
+	if [ -d ${BUILD_DIR}/_build/lib ]; then \
+		${CP} -rf ${BUILD_DIR}/_build/lib ${ADDON_DIR}/_build/; \
 	fi
-	if [ -d ${SRC_DIR}/_build/objs ]; then \
-		${CP} -rf ${SRC_DIR}/_build/objs ${TARGET_DIR}/_build/; \
+	if [ -d ${BUILD_DIR}/_build/objs ]; then \
+		${CP} -rf ${BUILD_DIR}/_build/objs ${ADDON_DIR}/_build/; \
 	fi
-	if [ -d ${SRC_DIR}/_build/headers ]; then \
-		${CP} -rf ${SRC_DIR}/_build/headers ${TARGET_DIR}/_build/; \
+	if [ -d ${BUILD_DIR}/_build/headers ]; then \
+		${CP} -rf ${BUILD_DIR}/_build/headers ${ADDON_DIR}/_build/; \
 	fi
-	if [ -d ${SRC_DIR}/depends ]; then \
-		${CP} -rf ${SRC_DIR}/depends ${TARGET_DIR}/; \
+	if [ -d ${BUILD_DIR}/io ]; then \
+		${CP} -rf ${BUILD_DIR}/io ${ADDON_DIR}/; \
 	fi
-	if [ -d ${SRC_DIR}/protos ]; then \
-		${CP} -rf ${SRC_DIR}/protos ${TARGET_DIR}/; \
+	if [ -d ${BUILD_DIR}/depends ]; then \
+		${CP} -rf ${BUILD_DIR}/depends ${ADDON_DIR}/; \
 	fi
-	if [ -d ${SRC_DIR}/io ]; then \
-		${CP} -rf ${SRC_DIR}/io ${TARGET_DIR}/; \
+	if [ -d ${BUILD_DIR}/protos ]; then \
+		${CP} -rf ${BUILD_DIR}/protos ${ADDON_DIR}/; \
 	fi
-	if [ -e ${SRC_DIR}/build.io ]; then \
-		${CP} -f ${SRC_DIR}/build.io ${TARGET_DIR}/; \
+	if [ -e ${BUILD_DIR}/build.io ]; then \
+		${CP} -f ${BUILD_DIR}/build.io ${ADDON_DIR}/; \
 	fi
 
