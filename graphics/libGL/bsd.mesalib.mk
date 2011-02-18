9fd74c95e6e858c1276b6fddf9d55428230b8348
Koop Mast <kwm@FreeBSD.org>
2011-02-18 16:50:33 +0000

    * Fix bsd.mesalibs.mk so PATCHDIR points to libGL/files * Move the EXTRA_PATCH for mesalib74-configure from the ports to bsd.mesalibs.mk.   remove mesali74-configure from all ports except libGL for obviously reasons.   Only dri and mesa-demos have own patches, didn't want to bother moving them. * The extra patches in libGL/files/patch-src* are build patches for trunk Clang.   And have been accepted upstream. Work done by dim@. * De-author pkg-descr's.

---
+++ graphics/libGL/bsd.mesalib.mk
@@ -0,0 +1,121 @@
+#-*- mode: Fundamental; tab-width: 4; -*-
+# ex:ts=4
+#
+# bsd.mesa.mk - an attempt to refactor MesaLib ports.
+#
+# Created by: Florent Thoumie <flz@FreeBSD.org>
+#
+# !!! Here be dragons !!! (they seem to be everywhere these days)
+#
+# Remember to upgrade the following ports everytime you bump MESAVERSION:
+#
+#    - graphics/libGL
+#    - graphics/libGLU
+#    - graphics/libGLw
+#    - graphics/libglut
+#    - graphics/dri
+#
+# $FreeBSD: ports/graphics/libGL/bsd.mesalib.mk,v 1.16 2010/02/07 11:51:05 nork Exp $
+#
+
+MESAVERSION=	${MESABASEVERSION}${MESASUBVERSION:C/^(.)/.\1/}
+MESADISTVERSION=${MESABASEVERSION}${MESASUBVERSION:C/^(.)/-\1/}
+
+.ifdef	WITHOUT_NOUVEAU
+MESABASEVERSION=	7.6.1
+MESASUBVERSION=
+PLIST_SUB+=		MESALIB76= MESALIB74="@comment "
+.else
+MESABASEVERSION=	7.4.4
+MESASUBVERSION=
+PLIST_SUB+=		MESALIB74= MESALIB76="@comment "
+EXTRA_PATCHES+=		${PATCHDIR}/mesalib74-configure
+.endif
+
+MASTER_SITES?=	ftp://ftp.freedesktop.org/pub/mesa/${MESABASEVERSION}/:mesa,glut,demos
+MASTER_SITE_SUBDIR=	mesa3d
+DISTFILES=		MesaLib-${MESADISTVERSION}${EXTRACT_SUFX}:mesa
+MAINTAINER?=	x11@FreeBSD.org
+
+USE_BZIP2=		yes
+USE_GMAKE=		yes
+USE_LDCONFIG=	yes
+GNU_CONFIGURE=	yes
+MAKE_JOBS_SAFE=	yes
+
+CONFIGURE_ENV=	CPPFLAGS=-I${LOCALBASE}/include \
+				LDFLAGS=-L${LOCALBASE}/lib
+CONFIGURE_ARGS=	--disable-gallium
+
+ALL_TARGET=		default
+
+#MAKE_ARGS=		SHELL=${SH}
+#CFLAGS+=		-DUSE_XSHM -DHZ=100
+
+PATCHDIR=		${.CURDIR}/../../graphics/libGL/files
+WRKSRC=			${WRKDIR}/Mesa-${MESABASEVERSION}
+
+.if !defined(ARCH)
+ARCH!=			uname -p
+.endif
+
+.if ${ARCH} == alpha
+FAST_MATH=
+.else
+FAST_MATH=      -ffast-math
+.endif
+
+COMPONENT=		${PORTNAME:L:C/^lib//:C/mesa-//}
+
+.if ${COMPONENT:Mglut} == ""
+. if ${COMPONENT:Mglu} == ""
+CONFIGURE_ARGS+=	--disable-glu --disable-glut
+. else
+CONFIGURE_ARGS+=	--disable-glut
+. endif
+.else
+DISTFILES+=		MesaGLUT-${MESADISTVERSION}${EXTRACT_SUFX}:glut
+.endif
+
+.if ${COMPONENT:Mglw} == ""
+CONFIGURE_ARGS+=	--disable-glw
+.else
+CONFIGURE_ARGS+=	--enable-motif
+.endif
+
+.if ${COMPONENT:Mdemos} == ""
+CONFIGURE_ARGS+=	--with-demos=no
+.else
+DISTFILES+=		MesaDemos-${MESADISTVERSION}${EXTRACT_SUFX}:demos
+CONFIGURE_ARGS+=	--with-demos=demos,xdemos
+.endif
+
+.if ${COMPONENT:Mdri} == ""
+CONFIGURE_ARGS+=	--with-dri-drivers=no
+.endif
+
+.if defined(WITHOUT_XCB)
+CONFIGURE_ARGS+=	--disable-xcb
+.else
+CONFIGURE_ARGS+=	--enable-xcb
+.endif
+
+post-patch:
+	@${REINPLACE_CMD} -e 's|-ffast-math|${FAST_MATH}|' -e 's|x86_64|amd64|' \
+		${WRKSRC}/configure
+	@${REINPLACE_CMD} -e 's|[$$](INSTALL_LIB_DIR)/pkgconfig|${PREFIX}/libdata/pkgconfig|' \
+		${WRKSRC}/src/glu/Makefile \
+		${WRKSRC}/src/glw/Makefile \
+		${WRKSRC}/src/mesa/Makefile \
+		${WRKSRC}/src/mesa/drivers/dri/Makefile
+.if ${COMPONENT:Mglut} != ""
+	@${REINPLACE_CMD} -e 's|[$$](INSTALL_LIB_DIR)/pkgconfig|${PREFIX}/libdata/pkgconfig|' \
+		${WRKSRC}/src/glut/glx/Makefile
+.endif
+.if ${COMPONENT:Mdemos} != ""
+	@${REINPLACE_CMD} -e 's|../images/|${DATADIR}/images/|g' \
+		-e 's|geartrain.dat|${DATADIR}/data/geartrain.dat|g' \
+		-e 's|terrain.dat|${DATADIR}/data/terrain.dat|g' \
+		-e 's|isosurf.dat|${DATADIR}/data/isosurf.dat|g' \
+			${WRKSRC}/progs/demos/*.c ${WRKSRC}/progs/xdemos/*.c
+.endif
