5d23e579743ac9c486bd101a1bb9943589a1b5e0
Niclas Zeising <zeising@FreeBSD.org>
2012-02-15 12:57:49 +0000

    Update mesa-demos to 8.0.1.
    This is the latest release of mesa-demos, allthough it is already ancient.  It
    gives us a place to start if we want to roll our own snapshots however.  Some
    testing might be needed, especially when  using WITH_NVIDIA, since I lack such
    hardware.  Some wordsmithing might also be good in pkg-descr.

--- graphics/mesa-demos/files/patch-src-xdemos-yuvrect_client.c
+++ graphics/mesa-demos/files/patch-src-xdemos-yuvrect_client.c
@@ -1,5 +1,5 @@
---- progs/xdemos/yuvrect_client.c.orig	Fri Apr  6 22:42:30 2007
-+++ progs/xdemos/yuvrect_client.c	Fri Apr  6 22:48:05 2007
+--- src/xdemos/yuvrect_client.c.orig	Fri Apr  6 22:42:30 2007
++++ src/xdemos/yuvrect_client.c	Fri Apr  6 22:48:05 2007
 @@ -140,7 +140,11 @@
        exit(0);
     }
