5cc7c4f17806f853f9cd40a0ec4ffeedb279bda9
Niclas Zeising <zeising@FreeBSD.org>
2013-08-14 12:21:31 +0000

    Merge r1055:
    Update to 2.21.14

--- x11-drivers/xf86-video-intel/files/extra-src_sna_compiler.h
+++ x11-drivers/xf86-video-intel/files/extra-src_sna_compiler.h
@@ -1,18 +1,27 @@
---- src/sna/compiler.h.orig	2013-03-12 12:48:29.745101779 +0100
-+++ src/sna/compiler.h	2013-03-12 12:48:52.857102997 +0100
+--- src/sna/compiler.h.orig	2013-08-01 21:00:51.000000000 +0200
++++ src/sna/compiler.h	2013-08-14 11:19:53.441221636 +0200
 @@ -37,7 +37,6 @@
  #define must_check __attribute__((warn_unused_result))
  #define constant __attribute__((const))
  #define pure __attribute__((pure))
 -#define __packed__ __attribute__((__packed__))
  #define flatten __attribute__((flatten))
+ #define page_aligned __attribute__((aligned(4096)))
  #else
- #define likely(expr) (expr)
-@@ -48,7 +47,6 @@
+@@ -49,7 +48,6 @@
  #define must_check
  #define constant
  #define pure
 -#define __packed__
  #define flatten
+ #define page_aligned
+ #endif
+@@ -67,6 +65,8 @@
+ 
+ #if HAS_GCC(4, 6) && defined(__OPTIMIZE__)
+ #define fast __attribute__((optimize("Ofast")))
++#else
++#define fast
  #endif
  
+ #if HAS_GCC(4, 6) && defined(__OPTIMIZE__)
