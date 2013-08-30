0b498f5fb8f0afd15b42db7a818c7b6c9157d31e
Niclas Zeising <zeising@FreeBSD.org>
2013-08-30 23:27:25 +0000

    Really plug that use before initialized.

--- x11-drivers/xf86-video-ati/files/extra-src__radeon_kms.c
+++ x11-drivers/xf86-video-ati/files/extra-src__radeon_kms.c
@@ -1,5 +1,14 @@
---- src/radeon_kms.c.orig	2013-08-30 17:34:45.402399526 +0200
-+++ src/radeon_kms.c	2013-08-30 17:36:31.138391972 +0200
+--- src/radeon_kms.c.orig	2013-08-07 10:44:09.000000000 +0200
++++ src/radeon_kms.c	2013-08-31 01:20:44.370468797 +0200
+@@ -270,7 +270,7 @@
+ radeon_dirty_update(ScreenPtr screen)
+ {
+ 	RegionPtr region;
+-	PixmapDirtyUpdatePtr ent;
++	PixmapDirtyUpdatePtr ent = NULL;
+ 
+ 	if (xorg_list_is_empty(&screen->pixmap_dirty_list))
+ 		return;
 @@ -606,7 +606,7 @@
  		      dev->domain, dev->bus, dev->dev, dev->func);
  #endif
