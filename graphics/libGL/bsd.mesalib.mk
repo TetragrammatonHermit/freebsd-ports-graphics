9a2788a89c70dc4d6d039e1fbcf29d2c327f67e1
Koop Mast <kwm@FreeBSD.org>
2013-11-26 20:15:55 +0000

    Add back python fix. To the !WITH_NEW_XORG case.

--- graphics/libGL/bsd.mesalib.mk
+++ graphics/libGL/bsd.mesalib.mk
@@ -124,6 +124,8 @@ post-patch:
 	@${REINPLACE_CMD} -e 's|/etc/|${PREFIX}/etc/|g' \
 		${WRKSRC}/src/mesa/drivers/dri/common/xmlconfig.c
 .if !defined(WITH_NEW_XORG)
+	@${REINPLACE_CMD} -e 's|python|${PYTHON_CMD}|' \
+		${WRKSRC}/src/gallium/auxiliary/util/Makefile
 	@${REINPLACE_CMD} -e 's|[$$](INSTALL_LIB_DIR)/pkgconfig|${PREFIX}/libdata/pkgconfig|' \
 		${WRKSRC}/src/glu/Makefile \
 		${WRKSRC}/src/mesa/Makefile \
