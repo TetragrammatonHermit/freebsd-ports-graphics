b11e7cba8d3f33deea359a57ebd6b7a7b28b028c
Niclas Zeising <zeising@FreeBSD.org>
2013-12-10 14:14:37 +0000

    Merge r1304-1306 from trunk:
    Fixes to libxshmfence.

--- x11/libxshmfence/files/patch-src__xshmfence_futex.h
+++ x11/libxshmfence/files/patch-src__xshmfence_futex.h
@@ -1,5 +1,5 @@
---- src/xshmfence_futex.h.orig	2013-11-20 17:13:08.000000000 -0500
-+++ src/xshmfence_futex.h	2013-12-05 16:09:39.000000000 -0500
+--- src/xshmfence_futex.h	2013-11-20 17:13:08.000000000 -0500
++++ src/xshmfence_futex.h	2013-12-09 15:52:29.000000000 -0500
 @@ -1,5 +1,6 @@
  /*
   * Copyright © 2013 Keith Packard
@@ -7,7 +7,7 @@
   *
   * Permission to use, copy, modify, distribute, and sell this software and its
   * documentation for any purpose is hereby granted without fee, provided that
-@@ -24,37 +25,59 @@
+@@ -24,6 +25,27 @@
  #define _XSHMFENCE_FUTEX_H_
  
  #include <errno.h>
@@ -17,14 +17,17 @@
 +#include <sys/types.h>
 +#include <sys/umtx.h>
 +
-+typedef u_long futex_type;
-+
-+static inline int futex_wake(futex_type *addr) {
-+	return umtx_wake(addr, INT_MAX);
++static inline int sys_futex(void *addr, int op, int32_t val)
++{
++	return _umtx_op(addr, op, (uint32_t)val, NULL, NULL) == -1 ? errno : 0;
 +}
 +
-+static inline int futex_wait(futex_type *addr, futex_type value) {
-+	return umtx_wait(addr, value, NULL);
++static inline int futex_wake(int32_t *addr) {
++	return sys_futex(addr, UMTX_OP_WAKE, INT_MAX);
++}
++
++static inline int futex_wait(int32_t *addr, int32_t value) {
++	return sys_futex(addr, UMTX_OP_WAIT_UINT, value);
 +}
 +
 +#else
@@ -32,23 +35,7 @@
  #include <stdint.h>
  #include <values.h>
  #include <linux/futex.h>
- #include <sys/time.h>
- #include <sys/syscall.h>
- 
-+typedef int32_t futex_type;
-+
- static inline long sys_futex(void *addr1, int op, int val1, struct timespec *timeout, void *addr2, int val3)
- {
- 	return syscall(SYS_futex, addr1, op, val1, timeout, addr2, val3);
- }
- 
--static inline int futex_wake(int32_t *addr) {
-+static inline int futex_wake(futex_type *addr) {
- 	return sys_futex(addr, FUTEX_WAKE, MAXINT, NULL, NULL, 0);
- }
- 
--static inline int futex_wait(int32_t *addr, int32_t value) {
-+static inline int futex_wait(futex_type *addr, futex_type value) {
+@@ -43,6 +65,8 @@
  	return sys_futex(addr, FUTEX_WAIT, value, NULL, NULL, 0);
  }
  
@@ -56,28 +43,4 @@
 +
  #define barrier() __asm__ __volatile__("": : :"memory")
  
--static inline void atomic_store(int32_t *f, int32_t v)
-+static inline void atomic_store(futex_type *f, futex_type v)
- {
- 	barrier();
- 	*f = v;
- 	barrier();
- }
- 
--static inline int32_t atomic_fetch(int32_t *a)
-+static inline futex_type atomic_fetch(futex_type *a)
- {
--	int32_t v;
-+	futex_type v;
- 	barrier();
- 	v = *a;
- 	barrier();
-@@ -62,7 +85,7 @@
- }
- 	
- struct xshmfence {
--    int32_t     v;
-+	futex_type v;
- };
- 
- #define xshmfence_init(fd)
+ static inline void atomic_store(int32_t *f, int32_t v)
