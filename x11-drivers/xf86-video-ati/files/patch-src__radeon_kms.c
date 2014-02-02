8f1685424765c1e4aa8fcd0c3efbf65246072a74
Koop Mast <kwm@FreeBSD.org>
2014-02-02 10:23:03 +0000

    Add back the radeonkms loading. The patch that landed upstream was the old fix which doesn't work for us. We probably need to chat with the kfreebsd people so see if this patch works for them too, instead of the patch that was committed.
    Reported by:	dumbbell@

--- x11-drivers/xf86-video-ati/files/patch-src__radeon_kms.c
+++ x11-drivers/xf86-video-ati/files/patch-src__radeon_kms.c
@@ -1,6 +1,15 @@
---- src/radeon_kms.c.orig	2013-12-24 21:13:42.000000000 +0100
-+++ src/radeon_kms.c	2014-01-25 15:48:21.000000000 +0100
-@@ -270,7 +270,7 @@
+--- src/radeon_kms.c.orig	2013-08-07 10:44:09.000000000 +0200
++++ src/radeon_kms.c	2013-08-31 19:29:11.369001510 +0200
+@@ -30,6 +30,8 @@
+ 
+ #include <errno.h>
+ #include <sys/ioctl.h>
++#include <sys/param.h>
++#include <sys/linker.h>
+ /* Driver data structures */
+ #include "radeon.h"
+ #include "radeon_reg.h"
+@@ -270,7 +272,7 @@
  radeon_dirty_update(ScreenPtr screen)
  {
  	RegionPtr region;
@@ -9,3 +18,20 @@
  
  	if (xorg_list_is_empty(&screen->pixmap_dirty_list))
  		return;
+@@ -606,6 +608,16 @@
+ 		      dev->domain, dev->bus, dev->dev, dev->func);
+ #endif
+ 
++    err = kldload("radeonkms");
++    if (err == -1 && errno != EEXIST) {
++
++	xf86DrvMsg(pScrn->scrnIndex, X_ERROR,
++		   "[drm] Failed to load kernel module for %s: %s\n",
++		   busid, strerror(errno));
++	free(busid);
++	return FALSE;
++    }
++
+     info->dri2.drm_fd = drmOpen("radeon", busid);
+     if (info->dri2.drm_fd == -1) {
+ 
